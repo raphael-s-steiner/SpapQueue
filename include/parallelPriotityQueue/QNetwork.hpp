cd6e172100acb44860bbbe1a305da8b0362c6465
Raphael Steiner <raphaelsteiner@gmail.com>
2025-08-18 19:48:55 +0200

    QNetwork and test complete

--- include/parallelPriotityQueue/QNetwork.hpp
+++ include/parallelPriotityQueue/QNetwork.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 
 namespace spapq {
@@ -12,15 +13,27 @@ struct QNetwork {
     std::array<std::size_t, workers + 1> vertexPointer_;
     std::array<std::size_t, workers> numPorts_;
     std::array<std::size_t, channels> edgeTargets_;
+    std::array<std::size_t, channels> multiplicities_;
     std::array<std::size_t, channels> targetPort_;
     std::array<std::size_t, channels> batchSize_;
 
+    constexpr void setDefaultMultiplicities();
     constexpr void setDefaultBatchSize();
     constexpr void assignTargetPorts();
 
-    constexpr QNetwork(std::array<std::size_t, workers + 1> vertexPointer, std::array<std::size_t, channels> edgeTargets, std::array<std::size_t, channels> batchSize) : numWorkers_(workers), numChannels_(channels), vertexPointer_(vertexPointer), edgeTargets_(edgeTargets), batchSize_(batchSize) { assignTargetPorts(); };
-    constexpr QNetwork(std::array<std::size_t, workers + 1> vertexPointer, std::array<std::size_t, channels> edgeTargets) : numWorkers_(workers), numChannels_(channels), vertexPointer_(vertexPointer), edgeTargets_(edgeTargets) { setDefaultBatchSize(); assignTargetPorts(); };
+    constexpr bool isValidQNetwork() const;
+
+    constexpr QNetwork(std::array<std::size_t, workers + 1> vertexPointer, std::array<std::size_t, channels> edgeTargets, std::array<std::size_t, channels> multiplicities, std::array<std::size_t, channels> batchSize) : numWorkers_(workers), numChannels_(channels), vertexPointer_(vertexPointer), edgeTargets_(edgeTargets), multiplicities_(multiplicities), batchSize_(batchSize) { assignTargetPorts(); };
+    constexpr QNetwork(std::array<std::size_t, workers + 1> vertexPointer, std::array<std::size_t, channels> edgeTargets, std::array<std::size_t, channels> multiplicities) : numWorkers_(workers), numChannels_(channels), vertexPointer_(vertexPointer), edgeTargets_(edgeTargets), multiplicities_(multiplicities) { setDefaultBatchSize(); assignTargetPorts(); };
+    constexpr QNetwork(std::array<std::size_t, workers + 1> vertexPointer, std::array<std::size_t, channels> edgeTargets) : numWorkers_(workers), numChannels_(channels), vertexPointer_(vertexPointer), edgeTargets_(edgeTargets) { setDefaultMultiplicities(); setDefaultBatchSize(); assignTargetPorts(); };
 };
+
+template<std::size_t workers, std::size_t channels>
+constexpr void QNetwork<workers, channels>::setDefaultMultiplicities() {
+    for (std::size_t i = 0U; i < multiplicities_.size(); ++i) {
+        multiplicities_[i] = 1U;
+    }
+}
 
 template<std::size_t workers, std::size_t channels>
 constexpr void QNetwork<workers, channels>::setDefaultBatchSize() {
@@ -37,6 +50,24 @@ constexpr void QNetwork<workers, channels>::assignTargetPorts() {
     for (std::size_t edge = 0U; edge < edgeTargets_.size(); ++edge) {
         targetPort_[edge] = numPorts_[edgeTargets_[edge]]++;
     }
+}
+
+template<std::size_t workers, std::size_t channels>
+constexpr bool QNetwork<workers, channels>::isValidQNetwork() const {
+    if ( not std::all_of(multiplicities_.cbegin(), multiplicities_.cend(), [](const std::size_t& mul) { return mul > 0U;}) ) {
+        return false;
+    }
+    if ( not std::all_of(batchSize_.cbegin(), batchSize_.cend(), [](const std::size_t& batch) { return batch > 0U;}) ) {
+        return false;
+    }
+
+    for (std::size_t worker = 0U; worker < numWorkers_; ++worker) {
+        if (vertexPointer_[worker] == vertexPointer_[worker + 1]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 } // end namespace spapq
--- tests/QNetwork.cpp
+++ tests/QNetwork.cpp
@@ -7,7 +7,7 @@
 using namespace spapq;
 
 TEST(QNetworkTest, Constructors1) {
-    constexpr QNetwork<4, 4> netw({0, 1, 2, 3, 4}, {1, 2, 3, 0}, {1, 2, 3, 4});
+    constexpr QNetwork<4, 4> netw({0, 1, 2, 3, 4}, {1, 2, 3, 0}, {10, 9, 8, 7}, {1, 2, 3, 4});
     EXPECT_EQ(netw.numWorkers_, 4);
     EXPECT_EQ(netw.numChannels_, 4);
     for (std::size_t i = 0; i < 5; ++i) {
@@ -15,6 +15,9 @@ TEST(QNetworkTest, Constructors1) {
     }
     for (std::size_t i = 0; i < 4; ++i) {
         EXPECT_EQ(netw.edgeTargets_[i], (i + 1) % 4);
+    }
+    for (std::size_t i = 0; i < 4; ++i) {
+        EXPECT_EQ(netw.multiplicities_[i], 10U - i);
     }
     for (std::size_t i = 0; i < 4; ++i) {
         EXPECT_EQ(netw.batchSize_[i], (i + 1));
@@ -33,6 +36,107 @@ TEST(QNetworkTest, Constructors2) {
         EXPECT_EQ(netw.edgeTargets_[i], (i + 1) % 4);
     }
     for (std::size_t i = 0; i < 4; ++i) {
+        EXPECT_EQ(netw.multiplicities_[i], 1);
+    }
+    for (std::size_t i = 0; i < 4; ++i) {
         EXPECT_EQ(netw.batchSize_[i], 1);
     }
+}
+
+TEST(QNetworkTest, Ports1) {
+    constexpr QNetwork<4, 4> netw({0, 1, 2, 3, 4}, {1, 2, 3, 0});
+    std::vector<std::vector<std::size_t>> outGraph(netw.numWorkers_);
+    std::vector<std::vector<std::size_t>> inGraph(netw.numWorkers_);
+
+    for (std::size_t src = 0; src < netw.numWorkers_; ++src) {
+        for (std::size_t tgtPtr = netw.vertexPointer_[src]; tgtPtr < netw.vertexPointer_[src + 1]; ++tgtPtr) {
+            const std::size_t tgt = netw.edgeTargets_[tgtPtr];
+            outGraph[src].push_back(tgt);
+            inGraph[tgt].push_back(src);
+        }
+    }
+
+    for (std::size_t worker = 0; worker < netw.numWorkers_; ++worker) {
+        EXPECT_EQ(netw.numPorts_[worker], inGraph[worker].size());
+    }
+
+    std::vector<std::vector<bool>> ports(netw.numWorkers_);
+    for (std::size_t worker = 0; worker < netw.numWorkers_; ++worker) {
+        ports[worker] = std::vector<bool>(inGraph[worker].size(), false);
+    }
+
+    for (std::size_t src = 0; src < netw.numWorkers_; ++src) {
+        for (std::size_t tgtPtr = netw.vertexPointer_[src]; tgtPtr < netw.vertexPointer_[src + 1]; ++tgtPtr) {
+            const std::size_t tgt = netw.edgeTargets_[tgtPtr];
+            const std::size_t port = netw.targetPort_[tgtPtr];
+            EXPECT_GE(port, 0U);
+            EXPECT_LT(port, inGraph[tgt].size());
+            EXPECT_FALSE(ports[tgt][port]);
+            ports[tgt][port] = true;
+        }
+    }
+
+    for (const auto &vec : ports) {
+        for (const bool &val : vec) {
+            EXPECT_TRUE(val);
+        }
+    }
+
+    EXPECT_TRUE(netw.isValidQNetwork());
+}
+
+TEST(QNetworkTest, Ports2) {
+    constexpr QNetwork<10, 30> netw(
+        {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30},
+        {
+            1, 5, 4,
+            2, 6, 0,
+            3, 7, 1,
+            4, 8, 2,
+            0, 9, 3,
+            6, 0, 9,
+            7, 1, 5,
+            8, 2, 6,
+            9, 3, 7,
+            5, 4, 8
+        }
+    );
+    std::vector<std::vector<std::size_t>> outGraph(netw.numWorkers_);
+    std::vector<std::vector<std::size_t>> inGraph(netw.numWorkers_);
+
+    for (std::size_t src = 0; src < netw.numWorkers_; ++src) {
+        for (std::size_t tgtPtr = netw.vertexPointer_[src]; tgtPtr < netw.vertexPointer_[src + 1]; ++tgtPtr) {
+            const std::size_t tgt = netw.edgeTargets_[tgtPtr];
+            outGraph[src].push_back(tgt);
+            inGraph[tgt].push_back(src);
+        }
+    }
+
+    for (std::size_t worker = 0; worker < netw.numWorkers_; ++worker) {
+        EXPECT_EQ(netw.numPorts_[worker], inGraph[worker].size());
+    }
+
+    std::vector<std::vector<bool>> ports(netw.numWorkers_);
+    for (std::size_t worker = 0; worker < netw.numWorkers_; ++worker) {
+        ports[worker] = std::vector<bool>(inGraph[worker].size(), false);
+    }
+
+    for (std::size_t src = 0; src < netw.numWorkers_; ++src) {
+        for (std::size_t tgtPtr = netw.vertexPointer_[src]; tgtPtr < netw.vertexPointer_[src + 1]; ++tgtPtr) {
+            const std::size_t tgt = netw.edgeTargets_[tgtPtr];
+            const std::size_t port = netw.targetPort_[tgtPtr];
+            EXPECT_GE(port, 0U);
+            EXPECT_LT(port, inGraph[tgt].size());
+            EXPECT_FALSE(ports[tgt][port]);
+            ports[tgt][port] = true;
+        }
+    }
+
+    for (const auto &vec : ports) {
+        for (const bool &val : vec) {
+            EXPECT_TRUE(val);
+        }
+    }
+
+    EXPECT_TRUE(netw.isValidQNetwork());
 }
