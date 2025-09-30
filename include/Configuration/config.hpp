34b988c8d3615e4249e9b933756377f7852f1cbc
Raphael Steiner <raphaelsteiner@gmail.com>
2025-09-30 15:15:23 +1000

    automatic cache line size

--- include/Configuration/config.hpp
+++ include/Configuration/config.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <cstddef>
+#include <new>
 
 namespace spapq
 {
 
-static constexpr std::size_t CACHE_LINE_SIZE = 64;
+#ifdef __cpp_lib_hardware_interference_size
+    static constexpr std::size_t CACHE_LINE_SIZE = std::hardware_destructive_interference_size;
+#else
+    static constexpr std::size_t CACHE_LINE_SIZE = 64U;
+#endif
 
 } // end namespace spapq
